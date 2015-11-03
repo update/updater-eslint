d232a1d789f2e044d0935852aeea33d245d1ad1d
jonschlinkert <jon.schlinkert@sellside.com>
2015-11-03 16:24:18 -0500

    move files to templates

--- updatefile.js
+++ updatefile.js
@@ -1,17 +1,29 @@
 'use strict';
 
+var fs = require('fs');
+var path = require('path');
 var extend = require('extend-shallow');
 
 module.exports = function (app, base, env) {
-  var config = require('./eslintrc.json');
+  var config = require(__dirname + '/templates/_eslintrc.json');
+
+  base.on('loaded', function(files) {
+    if (!files.getFile('.eslintrc')) {
+      var file = app.getFile('_eslintrc.json');
+      file.basename = '.eslintrc';
+      base.files('.eslintrc', file);
+    }
+  });
 
   base.onLoad(/\.eslint$/, function (file, next) {
-    file.json = extend({}, file.json, config);
+    if (file.json) {
+      file.json = extend({}, file.json, config);
+    }
     next();
   });
 
   base.onLoad(/package\.json/, function (file, next) {
-    if (!file.json.hasOwnPropert('eslintConfig')) {
+    if (!file.json.hasOwnProperty('eslintConfig')) {
       return next();
     }
 
