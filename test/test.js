6c547916c9239480e19b9ff1e6fe983c70bd1004
jonschlinkert <jon.schlinkert@sellside.com>
2016-07-21 21:08:29 -0400

    move/update tests

--- test/test.js
+++ test/test.js
@@ -4,31 +4,43 @@ require('mocha');
 var fs = require('fs');
 var path = require('path');
 var assert = require('assert');
-var Update = require('update');
+var update = require('update');
 var del = require('delete');
-var updater = require('./');
+var updater = require('..');
 var app;
 
-var cwd = path.resolve.bind(path, __dirname, 'actual');
+var actual = path.resolve.bind(path, __dirname, 'actual');
 
 function exists(name, cb) {
-  var filepath = cwd(name);
-
+  var filepath = actual(name);
   return function(err) {
     if (err) return cb(err);
 
     fs.stat(filepath, function(err, stat) {
+      if (err) return cb(err);
       assert(stat);
       del(path.dirname(filepath), cb);
+    });
+  }
+}
+function notExists(name, cb) {
+  var filepath = actual(name);
+  return function(err) {
+    if (err) return cb(err);
+
+    fs.stat(filepath, function(err, stat) {
+      assert(err);
+      assert(!stat);
+      del(actual(), cb);
     });
   }
 }
 
 describe('update-eslint', function() {
   beforeEach(function() {
-    app = new Update({silent: true});
-    app.cwd = cwd();
-    app.option('dest', cwd());
+    app = update({silent: true});
+    app.cwd = actual();
+    app.option('dest', actual());
   });
 
   describe('plugin', function() {
@@ -59,14 +71,19 @@ describe('update-eslint', function() {
       app.update('default', exists('.eslintrc.json', cb));
     });
 
-    it.skip('should run the `ignore` task', function(cb) {
+    it('should run the `new` task', function(cb) {
       app.use(updater);
-      app.update('ignore', exists('.eslintignore', cb));
+      app.update('eslint:eslint-new', exists('.eslintrc.json', cb));
     });
 
-    it.skip('should run the `eslintignore` (alias) task', function(cb) {
+    it('should run the `new` and `del` tasks', function(cb) {
       app.use(updater);
-      app.update('eslintignore', exists('.eslintignore', cb));
+      app.update('eslint', ['new', 'del'], notExists('.eslintrc.json', cb));
+    });
+
+    it('should run the `eslint-new` and `eslint-del` tasks', function(cb) {
+      app.use(updater);
+      app.update('eslint', ['eslint-new', 'eslint-del'], notExists('.eslintrc.json', cb));
     });
   });
 
@@ -81,14 +98,14 @@ describe('update-eslint', function() {
       app.update('eslint:default', exists('.eslintrc.json', cb));
     });
 
-    it.skip('should run the `ignore` task', function(cb) {
+    it('should run the `new` task', function(cb) {
       app.register('eslint', updater);
-      app.update('eslint:ignore', exists('.eslintignore', cb));
+      app.update('eslint:new', exists('.eslintrc.json', cb));
     });
 
-    it.skip('should run the `eslintignore` (alias) task', function(cb) {
+    it('should run the `new` (alias) task', function(cb) {
       app.register('eslint', updater);
-      app.update('eslint:eslintignore', exists('.eslintignore', cb));
+      app.update('eslint:new', exists('.eslintrc.json', cb));
     });
   });
 
@@ -107,18 +124,18 @@ describe('update-eslint', function() {
       app.update('foo.eslint:default', exists('.eslintrc.json', cb));
     });
 
-    it.skip('should run the `ignore` task', function(cb) {
+    it('should run the `new` task', function(cb) {
       app.register('foo', function(foo) {
         foo.register('eslint', updater);
       });
-      app.update('foo.eslint:ignore', exists('.eslintignore', cb));
+      app.update('foo.eslint:new', exists('.eslintrc.json', cb));
     });
 
-    it.skip('should run the `eslintignore` (alias) task', function(cb) {
+    it('should run the `new` (alias) task', function(cb) {
       app.register('foo', function(foo) {
         foo.register('eslint', updater);
       });
-      app.update('foo.eslint:eslintignore', exists('.eslintignore', cb));
+      app.update('foo.eslint:new', exists('.eslintrc.json', cb));
     });
   });
 });
